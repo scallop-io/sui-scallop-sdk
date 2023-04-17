12ad851baea0f821b89c378319a81e38d4586158
Nathan Ramli <hezkyanatanael84@gmail.com>
2023-04-18 00:28:30 +0700

    fix: wrong params

--- example/open-obligation.ts
+++ example/open-obligation.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import { ScallopSui } from '../src';
-import { packageId, marketId, coinDecimalsRegistryId, adminCapId } from './object-ids';
+import { packageId, marketId, coinDecimalsRegistryId, adminCapId, priceFeedsId } from './object-ids';
 import { SHINAMI_DEVNET_FULLNODE } from './shinami-fullnodes'
 dotenv.config();
 
@@ -10,6 +10,7 @@ dotenv.config();
     marketId,
     coinDecimalsRegistryId,
     adminCapId,
+    priceFeedsId,
     suiConfig: {
       mnemonics: process.env.MNEMONICS,
       fullnodeUrl: SHINAMI_DEVNET_FULLNODE
