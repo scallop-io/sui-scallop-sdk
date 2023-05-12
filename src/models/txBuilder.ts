db4dc316c70846b736630447a0a78a7bb59900ac
Donnie <donor.gm@gmail.com>
2023-05-13 05:55:44 +0800

    feat: add query market data and its unit tests

--- src/models/txBuilder.ts
+++ src/models/txBuilder.ts
@@ -1,6 +1,9 @@
 import { SuiTxBlock } from '@scallop-io/sui-kit';
 import type { TxBuilderParams } from 'src/types/model';
 
+/**
+ * it provides methods for build transaction.
+ */
 export class ScallopTxBuilder {
   public suiTxBlock: SuiTxBlock;
 
@@ -8,7 +11,25 @@ export class ScallopTxBuilder {
     this.suiTxBlock = new SuiTxBlock();
   }
 
+  /**
+   * Get transaction block.
+   *
+   * @returns sui type transaction block.
+   */
   public get txBlock() {
     return this.suiTxBlock.txBlock;
+  }
+
+  /**
+   * Construct a transaction block for querying market data.
+   *
+   * @param packageId - The query package id.
+   * @param marketId - The market id from protocol package.
+   * @returns Sui-Kit type transaction block.
+   */
+  public queryMarket(packageId: string, marketId: string) {
+    const queryTarget = `${packageId}::market_query::market_data`;
+    this.suiTxBlock.moveCall(queryTarget, [marketId]);
+    return this.suiTxBlock;
   }
 }
