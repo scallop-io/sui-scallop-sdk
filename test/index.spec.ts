b2e2f8aa85f6d600db66cf5c3e285360939de89f
Donnie <donor.gm@gmail.com>
2023-05-13 19:00:22 +0800

    feat: add open & query obligations and its unit tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,20 +7,37 @@ dotenv.config();
 
 const NETWORK: NetworkType = 'testnet';
 
-describe('Test Scallop interact with contract', () => {
+describe('Test Scallop interact with contract', async () => {
   const scallopSDK = new Scallop({
     secretKey: process.env.SECRE_KEY,
     networkType: NETWORK,
   });
+  const client = await scallopSDK.createScallopClient();
 
   it('Should get Scallop instance', async () => {
     expect(!!scallopSDK).toBe(true);
   });
 
   it('Should get market query data', async () => {
-    const client = await scallopSDK.createScallopClient();
     const marketData = await client.queryMarket();
-    console.log(marketData);
+    console.info('marketData:', marketData);
     expect(!!marketData).toBe(true);
+  });
+
+  it.skip('Should open a obligation account', async () => {
+    const openObligationResult = await client.openObligation();
+    console.info('openObligationResult:', openObligationResult);
+    expect(openObligationResult.effects.status.status).toEqual('success');
+  });
+
+  it('Should get obligations and its query data', async () => {
+    const obligations = await client.getObligations();
+    console.info('obligations', obligations);
+
+    for (const { id } of obligations) {
+      const obligationData = await client.queryObligation(id);
+      console.info('obligationData', obligationData);
+      expect(!!obligationData).toBe(true);
+    }
   });
 });
