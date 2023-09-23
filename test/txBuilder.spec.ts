d22c22fc4c4aacfd672c5401994270bf1a5b88e8
Donnie <donor.gm@gmail.com>
2023-09-23 14:58:00 +0800

    refactor: remove override addresses in txBuilder test

--- test/txBuilder.spec.ts
+++ test/txBuilder.spec.ts
@@ -19,54 +19,6 @@ describe('Test Scallop transaction builder', async () => {
   const txBuilder = await scallopSDK.createTxBuilder();
   console.info('\x1b[32mSender: \x1b[33m', sender);
 
-  // override pyth oracle addresses
-  scallopSDK.address.set(
-    'core.packages.pyth.id',
-    '0xbab81459a9388dc39e89fc780a300191b1b8bc38f4ecfa1ad050d3bdc52ad8af'
-  );
-  scallopSDK.address.set(
-    'core.oracles.pyth.registry',
-    '0xec603dcaf540bb747dddc961ca9e019e1f0a138d12121d350ef53a1096fce55a'
-  );
-  scallopSDK.address.set(
-    'core.oracles.pyth.state',
-    '0x1f9310238ee9298fb703c3419030b35b22bb1cc37113e3bb5007c99aec79e5b8'
-  );
-
-  // override oracle feed addresses
-  scallopSDK.address.set(
-    'core.coins.sui.oracle.pyth.feedObject',
-    '0x801dbc2f0053d34734814b2d6df491ce7807a725fe9a01ad74a07e9c51396c37'
-  );
-  scallopSDK.address.set(
-    'core.coins.usdc.oracle.pyth.feedObject',
-    '0x5dec622733a204ca27f5a90d8c2fad453cc6665186fd5dff13a83d0b6c9027ab'
-  );
-  scallopSDK.address.set(
-    'core.coins.usdt.oracle.pyth.feedObject',
-    '0x985e3db9f93f76ee8bace7c3dd5cc676a096accd5d9e09e9ae0fb6e492b14572'
-  );
-  scallopSDK.address.set(
-    'core.coins.eth.oracle.pyth.feedObject',
-    '0x9193fd47f9a0ab99b6e365a464c8a9ae30e6150fc37ed2a89c1586631f6fc4ab'
-  );
-  scallopSDK.address.set(
-    'core.coins.btc.oracle.pyth.feedObject',
-    '0x9a62b4863bdeaabdc9500fce769cf7e72d5585eeb28a6d26e4cafadc13f76ab2'
-  );
-  scallopSDK.address.set(
-    'core.coins.sol.oracle.pyth.feedObject',
-    '0x9d0d275efbd37d8a8855f6f2c761fa5983293dd8ce202ee5196626de8fcd4469'
-  );
-  scallopSDK.address.set(
-    'core.coins.cetus.oracle.pyth.feedObject',
-    '0x24c0247fb22457a719efac7f670cdc79be321b521460bd6bd2ccfa9f80713b14'
-  );
-  scallopSDK.address.set(
-    'core.coins.apt.oracle.pyth.feedObject',
-    '0x7c5b7837c44a69b469325463ac0673ac1aa8435ff44ddb4191c9ae380463647f'
-  );
-
   test('"openObligationEntry" should create a shared obligation, and send obligationKey to sender', async () => {
     const tx = txBuilder.createTxBlock();
     tx.openObligationEntry();
