13c660d8a3c4b48a24caecdec008231d29497174
fum-is-chum <alvinardiansyah2002@gmail.com>
2024-06-10 03:18:43 +0700

    feat: add loyalty program

--- src/types/builder/index.ts
+++ src/types/builder/index.ts
@@ -3,14 +3,17 @@ import type { SpoolTxBlock } from './spool';
 import type { BorrowIncentiveTxBlock } from './borrowIncentive';
 import type { VeScaTxBlock } from './vesca';
 import type { ReferralTxBlock } from './referral';
+import { LoyaltyProgramTxBlock } from './loyaltyProgram';
 
 export type * from './core';
 export type * from './spool';
 export type * from './borrowIncentive';
 export type * from './vesca';
+export type * from './loyaltyProgram';
 
 export type ScallopTxBlock = CoreTxBlock &
   SpoolTxBlock &
   ReferralTxBlock &
+  LoyaltyProgramTxBlock &
   BorrowIncentiveTxBlock &
   VeScaTxBlock;
