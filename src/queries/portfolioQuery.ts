faa647d7eac2518a71fb12eb4060bdf42c9d2ceb
fum-is-chum <alvinardiansyah2002@gmail.com>
2024-07-09 11:25:11 +0700

    fix: plus sCoinAmount into unstakedMarketAmount

--- src/queries/portfolioQuery.ts
+++ src/queries/portfolioQuery.ts
@@ -219,7 +219,9 @@ export const getLending = async (
   const marketCoinPrice = BigNumber(coinPrice ?? 0).multipliedBy(
     marketPool?.conversionRate ?? 1
   );
-  const unstakedMarketAmount = BigNumber(marketCoinAmount);
+  const unstakedMarketAmount = BigNumber(marketCoinAmount).plus(
+    BigNumber(sCoinAmount)
+  );
   const unstakedMarketCoin = unstakedMarketAmount.shiftedBy(-1 * coinDecimal);
 
   const availableSupplyAmount = BigNumber(coinAmount);
