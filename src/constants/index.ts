e3f7b4d420a2124a916048fc4011b420025a84f1
fum-is-chum <alvinardiansyah2002@gmail.com>
2024-12-27 13:21:42 +0800

    feat: merge token bucket into scallopCache

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,6 +8,5 @@ export * from './pyth';
 export * from './queryKeys';
 export * from './rpc';
 export * from './testAddress';
-export * from './tokenBucket';
 export * from './vesca';
 export * from './testAddress';
