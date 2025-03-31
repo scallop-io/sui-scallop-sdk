b713bb13d5aff02390cd8f80d8e520199197e9e2
fum-is-chum <alvinardiansyah2002@gmail.com>
2025-03-31 23:46:01 +0700

    fix: handling boolean values for pool address

--- src/models/scallopConstants.ts
+++ src/models/scallopConstants.ts
@@ -404,11 +404,16 @@ export class ScallopConstants {
           )
           .map(([key, value]) => {
             const parsedValue = Object.fromEntries(
-              Object.entries(value).map(([k, v]) => [k, v || undefined])
+              Object.entries(value).map(([k, v]) => [
+                k,
+                typeof v === 'boolean' ? (v ?? false) : v || undefined,
+              ])
             );
             return [key, parsedValue as PoolAddress];
           })
       );
     }
+
+    console.log(poolAddressesResponse.wal, this.poolAddresses.wal);
   }
 }
