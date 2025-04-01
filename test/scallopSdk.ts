762d6212cb1f0c27809e8b7f312b566de7ffb7f1
fum-is-chum <alvinardiansyah2002@gmail.com>
2025-04-01 18:00:05 +0700

    test: use API

--- test/scallopSdk.ts
+++ test/scallopSdk.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 import { NetworkType } from '@scallop-io/sui-kit';
 import { Scallop } from 'src';
-import { POOL_ADDRESSES, WHITELIST } from './mocks';
+// import { POOL_ADDRESSES, WHITELIST } from './mocks';
 dotenv.config();
 
 const NETWORK: NetworkType = 'mainnet';
@@ -9,6 +9,6 @@ export const scallopSDK = new Scallop({
   addressId: '67c44a103fe1b8c454eb9699',
   secretKey: process.env.SECRET_KEY,
   networkType: NETWORK,
-  forcePoolAddressInterface: POOL_ADDRESSES,
-  forceWhitelistInterface: WHITELIST,
+  // forcePoolAddressInterface: POOL_ADDRESSES,
+  // forceWhitelistInterface: WHITELIST,
 });
