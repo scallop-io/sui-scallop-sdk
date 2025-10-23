f5118bb2b7f19d0e006d9cb72a9eaea062ce8f14
Alvin <alvinardiansyah2002@gmail.com>
2025-10-23 09:52:54 +0700

    feat: remove blast api

--- src/constants/rpc.ts
+++ src/constants/rpc.ts
@@ -2,7 +2,6 @@ import { getFullnodeUrl } from '@mysten/sui/client';
 
 export const RPC_PROVIDERS = [
   getFullnodeUrl('mainnet'),
-  'https://sui-mainnet.public.blastapi.io',
   'https://sui-mainnet-ca-2.cosmostation.io',
   'https://sui-mainnet-eu-4.cosmostation.io',
   'https://sui-mainnet-endpoint.blockvision.org',
